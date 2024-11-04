bb026a7956e5e0aeeca749344a032bae9f79f3fd
MITCHYUGAN <denmichigan123@gmail.com>
2024-11-04 13:22:25 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Forms from './components/forms/Forms'
+import Resume from './components/resume/Resume'
+
+function App() {
+
+  return (
+    <main className='main'>
+      <Forms />
+      <Resume />
+    </main>
+  )
+}
+
+export default App
