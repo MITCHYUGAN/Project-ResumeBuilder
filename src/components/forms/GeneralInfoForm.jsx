78d0705bc1ed3c2aa0a77ecc52946c9b9f50c86f
MITCHYUGAN <denmichigan123@gmail.com>
2024-11-04 19:02:46 +0100

    feat: General Info on the Resume is now updating it values based on the GeneralInfo Form and necessary styling is applied

--- src/components/forms/GeneralInfoForm.jsx
+++ src/components/forms/GeneralInfoForm.jsx
@@ -1,39 +1,50 @@
+import { useState } from "react"
+
 /* eslint-disable react/prop-types */
 const GeneralInfoForm = ({ generalInfo, setGeneralInfo }) => {
 
+  const [openGeneralInfo, setopenGeneralInfo] = useState({
+    open: true,
+    icon: "▼"
+  })
+
   return (
     <section aria-label="General Information" className="generalInfo">
-      <div className='generalInfoHeading'>
+      <div className='generalInfoHeading' onClick={() => {
+          openGeneralInfo.open ? setopenGeneralInfo({...openGeneralInfo, open: false, icon: "▲"}) : setopenGeneralInfo({...openGeneralInfo, open: true, icon: "▼"})
+        }}>
         <h1>General Information</h1>
-        {/* <p>&and;</p> */}
-        <p>▼</p>
-        {/* <p>&or;</p> */}
+        <button>
+          <p aria-hidden="true">{openGeneralInfo.icon}</p>
+        </button>
       </div>
-      <form action="">
+      {
+        openGeneralInfo.open && <form action="">
         <label htmlFor="full-name">Full Name</label>
-        <input type="text" name="full-name" id="full-name" placeholder='John Snow'
+        <input type="text" name="full-name" id="full-name" value={generalInfo.name} placeholder='John Snow'
           onChange={(e) => setGeneralInfo({ ...generalInfo, name: e.target.value })}
         />
         <label htmlFor="position">Position</label>
-        <input type="text" id='position' name='position' placeholder='Frontend Developer'
+        <input type="text" id='position' name='position' value={generalInfo.position} placeholder='Frontend Developer'
           onChange={(e) => setGeneralInfo({ ...generalInfo, position: e.target.value })}
         />
         <label htmlFor="bio">Bio</label>
-        <textarea name="bio" id="bio" placeholder='Frontend Developer Proficient in HTML, CSS, Java-Script and React. I specialize in creaing efficient and responsive website that meet organizations needs'
+        <textarea name="bio" id="bio" value={generalInfo.bio} placeholder='Frontend Developer Proficient in HTML, CSS, Java-Script and React. I specialize in creaing efficient and responsive website that meet organizations needs'
           onChange={(e) => setGeneralInfo({ ...generalInfo, bio: e.target.value })}></textarea>
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" placeholder='generalemail@gmail.com'
+        <input type="email" name="email" value={generalInfo.email} id="email" placeholder='generalemail@gmail.com'
           onChange={(e) => setGeneralInfo({ ...generalInfo, email: e.target.value })}
         />
         <label htmlFor="socials">Social Link</label>
-        <input type="text" name="socials" id="socials" placeholder='https://x.com/me'
+        <input type="text" name="socials" id="socials" value={generalInfo.social} placeholder='https://x.com/me'
           onChange={(e) => setGeneralInfo({ ...generalInfo, social: e.target.value })}
         />
         <label htmlFor="phoneNo">Phone Number</label>
-        <input type="tel" name="phoneNo" id="phoneNo" placeholder='+23480273655'
+        <input type="tel" name="phoneNo" id="phoneNo" value={generalInfo.phoneNo} placeholder='+23480273655'
           onChange={(e) => setGeneralInfo({ ...generalInfo, phoneNo: e.target.value })}
         />
       </form>
+      }
     </section>
   )
 }
